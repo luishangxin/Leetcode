5262a14ea6c08b2689bb4c81e9cc826bf203e0ac
e <luihero@126.com>
2017-11-21 20:58:22 +0800

    C11

---
+++ Leetcode125.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        auto left = s.begin(), right = prev(s.end());
+	while (left < right) {	 
+	    if (!::isalnum(*left)) ++left;	
+	    else if (!::isalnum(*right)) --right;
+	    else if (tolower(*left++) != tolower(*right--)) return false;
+        }
+	return true;
+									    }
+									    };
